294357234cdc4574f5cdb1f3e75f09678b1373da
Lee geon woo <78680884+Geonwoo-Lee@users.noreply.github.com>
2021-09-12 19:15:23 +0900

    설명서

--- src/App.js
+++ src/App.js
@@ -7,6 +7,10 @@ function App() {
     hi
     myname is geonwoo
     and it will be end of this practice
+
+    1 프로젝트 마스터가 main과 develope 과 다른 브랜치들을 만들어 놓은 뒤에
+    main에 있는 애들을 git clone 으로 복사해서 넣어 놓는다 . 
+    2. 협업자들은 develope과 다른 브랜치들을 fork clone 해서 작업 후  commit과 push 진행 한다 그다음에는 검토 후에 develope의 파일들이 정상적이라면 merge 한다 
     </div>
   );
 }
